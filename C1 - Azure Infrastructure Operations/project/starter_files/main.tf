db5c5d154583eab4b5832132c235b7f60566e2ba
fehlen501602 <62117296+fehlen501602@users.noreply.github.com>
2022-08-07 21:49:59 +0200

    resub

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
-  features {}
+  features {
+      resource_group {
+    prevent_deletion_if_contains_resources = false
+  }
+  }
 
   //subscription_id = "60f90b24-74e9-49ca-bd84-cb47478cc5e5"
 }
@@ -8,7 +12,7 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
   tags = {
-    Name  = var.tags
+    environment  = var.tags
   }
 }
 
@@ -18,7 +22,7 @@ resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   tags = {
-    Name  = var.tags
+    environment  = var.tags
   }
 }
 
@@ -60,7 +64,7 @@ resource "azurerm_network_security_group" "main" {
   }
 
   tags = {
-    Name  = var.tags
+    environment  = var.tags
   }
 }
 
@@ -70,12 +74,26 @@ resource "azurerm_public_ip" "public_ip" {
   location            = azurerm_resource_group.main.location
   allocation_method   = "Dynamic"
   tags = {
-    Name  = var.tags
+    environment  = var.tags
+  }
+}
+
+resource "azurerm_availability_set" "avset" {
+  name                = "${var.prefix}-avset"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  platform_fault_domain_count = 2
+  platform_update_domain_count = 2
+  managed = true
+
+  tags = {
+    environment = var.tags
   }
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+  count               = var.number
+  name                = "${var.prefix}-nic-${count.index}"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
 
@@ -85,7 +103,7 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_allocation = "Dynamic"
   }
   tags = {
-    Name  = var.tags
+    environment  = var.tags
   }
 }
 
@@ -93,21 +111,36 @@ resource "azurerm_lb" "main" {
   name                = "ProjectLoadBalancer"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
 
   tags = {
-    Name  = var.tags
+    environment  = var.tags
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "main" {
   loadbalancer_id = azurerm_lb.main.id
-  name            = "classiclb"
+  name            = "BackEndAddressPool"
 
 }
 
+resource "azurerm_lb_nat_rule" "main" {
+  resource_group_name            = azurerm_resource_group.main.name
+  loadbalancer_id                = azurerm_lb.main.id
+  name                           = "HTTPAccess"
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 443
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
 resource "azurerm_network_interface_backend_address_pool_association" "main" {
-  network_interface_id    = azurerm_network_interface.main.id
+  count                   = var.number
+  network_interface_id    = element(azurerm_network_interface.main.*.id, count.index)
   ip_configuration_name   = "internal"
   backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
 
@@ -118,15 +151,16 @@ resource "azurerm_linux_virtual_machine" "main" {
   name                            = "${var.prefix}-vm-${count.index}"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
+  availability_set_id             = azurerm_availability_set.avset.id
   size                            = "Standard_D2s_v3"
   admin_username                  = "${var.username}"
   admin_password                  = "${var.password}"
   disable_password_authentication = false
   network_interface_ids = [
-    azurerm_network_interface.main.id,
+    azurerm_network_interface.main[count.index].id, 
   ]
   tags = {
-    Name  = var.tags
+    environment  = var.tags
   }
 
   source_image_reference {
@@ -137,6 +171,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   }
 
   os_disk {
+    name                 = "${var.prefix}OS-${count.index}"
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
--- C1 - Azure Infrastructure Operations/project/starter_files/var.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/var.tf
@@ -1,23 +1,29 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default = "project"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "West Europe"
 }
 
 variable "username" {
   description = "The administration username"
+  default = "testadmin"
 }
 
 variable "password" {
   description = "The administration password."
+  default =  "Password1234!"
 
 }
 variable "tags" {
   description = "The tags for the resources"
+  default = "environment"
 }
 
 variable "number" {
   description = "The number of the virtual machines"
+  default = 2
 }
